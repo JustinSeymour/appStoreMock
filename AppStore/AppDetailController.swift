7d9eafaef83a7f352dc4894ab3b8432190dc693e
Justin Seymour <justin@3wmholdings.com>
2017-12-18 18:39:48 +0200

    Espisode $

---
+++ AppStore/AppDetailController.swift
@@ -0,0 +1,141 @@
+//
+//  AppDetailController.swift
+//  AppStore
+//
+//  Created by Justin Seymour on 2017/12/18.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    var app: App? {
+        didSet {
+            navigationItem.title = app?.name
+        }
+    }
+    
+    fileprivate let headerId = "headerId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.alwaysBounceVertical = true
+        
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        collectionView?.register(AppDetailHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+       
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! AppDetailHeader
+        header.app = app
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 170)
+    }
+}
+
+class AppDetailHeader: BaseCell {
+    
+    var app: App? {
+        didSet {
+            if let imageName = app?.imageName {
+                imageView.image = UIImage(named: imageName)
+            }
+            nameLabel.text = app?.name
+            
+            if let price = app?.price?.stringValue {
+                buyButton.setTitle("$\(price)", for: .normal)
+            }
+        }
+    }
+    
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 16
+        iv.layer.masksToBounds = true
+        return iv
+    }()
+    
+    let segmentedControl: UISegmentedControl = {
+        let sc = UISegmentedControl(items: ["Details", "Reviews", "Related"])
+        sc.tintColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        sc.selectedSegmentIndex = 0
+        return sc
+    }()
+    
+    let nameLabel: UILabel = {
+       let label = UILabel()
+       label.text = "TEST"
+       label.font = UIFont.systemFont(ofSize: 16)
+       return label
+    }()
+    
+    let buyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("BUY", for: .normal)
+        button.layer.borderColor = UIColor(red: 0, green: 120/255, blue: 250/255, alpha: 1).cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
+    override func setUpViews() {
+        super.setUpViews()
+        
+        addSubview(imageView)
+        addSubview(segmentedControl)
+        addSubview(nameLabel)
+        addSubview(buyButton)
+        
+        addConstraintsWithFormat("H:|-14-[v0(100)]-8-[v1]|", views: imageView, nameLabel)
+        addConstraintsWithFormat("V:|-14-[v0(100)]", views: imageView)
+        addConstraintsWithFormat("V:|-14-[v0(20)]", views: nameLabel)
+        addConstraintsWithFormat("H:|-40-[v0]-40-|", views: segmentedControl)
+        addConstraintsWithFormat("V:[v0(34)]-8-|", views: segmentedControl)
+        
+        
+        addConstraintsWithFormat("H:[v0(60)]-14-|", views: buyButton)
+        addConstraintsWithFormat("V:[v0(32)]-56-|", views: buyButton)
+        
+    }
+    
+}
+
+extension UIView {
+    
+    func addConstraintsWithFormat(_ format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
+
+class BaseCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews() {
+        
+    }
+}
+
+
